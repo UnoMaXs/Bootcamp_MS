c64d613548fd00c8fae716e1a7712717784b00e1
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-06-12 15:41:22 -0500

    Proyecto base

---
+++ src/main/java/com/bootcamp_ms/BootcampMsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bootcamp_ms/BootcampMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
