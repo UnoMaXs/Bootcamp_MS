d8100be0ed57d8182c69008929bb0fdb50efcf04
pierre.rojas_pragma <pier.rojas@pragma.com.co>
2025-06-13 14:34:49 -0500

    "Yo como admin necesito registrar bootcamps para dar inicio a eventos del bootcamp" "-cada bootcamp debe tener su respectivo id,nombre,descripcion, fecha de lanzamiento, duracion y un listado de capacidades asociadas. -un bootcamp debe tener como minimo 1 capacidad asociadas y como maximo 4."

---
+++ src/main/java/com/bootcamp_ms/application/UseCasesConfig.java
@@ -0,0 +1,30 @@
+package com.bootcamp_ms.application;
+
+import com.bootcamp_ms.domain.api.IBootcampServicePort;
+import com.bootcamp_ms.domain.spi.IBootcampPersistencePort;
+import com.bootcamp_ms.domain.usecase.BootcampUseCase;
+import com.bootcamp_ms.infrastructure.adapters.mapper.IBootcampMapper;
+import com.bootcamp_ms.infrastructure.adapters.persistence.BootcampAdapter;
+import com.bootcamp_ms.infrastructure.adapters.repository.IBootcampRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class UseCasesConfig {
+
+    private final IBootcampRepository bootcampRepository;
+    private final IBootcampMapper bootcampMapper;
+
+
+    @Bean
+    public IBootcampPersistencePort bootcampPersistencePort() {
+        return new BootcampAdapter(bootcampRepository, bootcampMapper);
+    }
+
+    @Bean
+    public IBootcampServicePort bootcampServicePort(IBootcampPersistencePort bootcampPersistencePort) {
+        return new BootcampUseCase(bootcampPersistencePort);
+    }
+}
